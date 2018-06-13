bb61331200d1d44cc82da4902bcc5a480befcaf8
Israel Levin <israel@paket.global>
2018-06-13 10:23:47 +0300

    Linting.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 """Install paket-stellar package"""
 from setuptools import setup
 
-setup(name='paket-stellar',
-      description='Stellar operations for the PAKET project',
-      version='1.0.0',
-      url='https://github.com/paket-core/paket-stellar',
-      license='GNU GPL',
-      packages=['paket_stellar'],
-      install_requires=[
-            'requests==2.18.4'
-      ],
-      test_suite='tests',
-      zip_safe=False)
+setup(
+    name='paket-stellar',
+    description='Stellar operations for the PAKET project',
+    version='1.0.0',
+    url='https://github.com/paket-core/paket-stellar',
+    license='GNU GPL',
+    packages=['paket_stellar'],
+    install_requires=[
+        'requests==2.18.4'
+    ],
+    test_suite='tests',
+    zip_safe=False)
